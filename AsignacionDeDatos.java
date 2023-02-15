43dbae2f3c5acdec479fa05db25f1df529e31e04
carlos enrique rivera olguin <riveraocarlosenrique@gmail.com>
2023-02-14 19:20:03 -0600

    Primer Uso

---
+++ AsignacionDeDatos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asignacion.de.datos;
+
+/**
+ *
+ * @author river
+ */
+public class AsignacionDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    int numero;
+    double decimales;
+    boolean desicion;
+    float rendimiento;
+    byte calificacion;
+    String cadena="Carlos";
+    
+}
